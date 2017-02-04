a70db0315c6240df8705f4fa8e586f3b91a58cc4
James Novak <jamesnovak90@gmail.com>
2017-02-04 18:04:19 -0500

    added time complexities

--- data-structures/queue.js
+++ data-structures/queue.js
@@ -5,7 +5,7 @@ function Queue(capacity) {
   this.tail = 0
 }
 
-// Time complexity:
+// Time complexity: O(1)
 Queue.prototype.enqueue = function(value) {
   if(this.count() < this.capacity){
     this.storage[this.tail++] = value
@@ -14,7 +14,7 @@ Queue.prototype.enqueue = function(value) {
   return 'Max capacity reached. Remove an item first'
 }
 
-// Time complexity:
+// Time complexity: O(1)
 Queue.prototype.dequeue = function() {
   var element = this.storage[this.head]
   delete this.storage[this.head]
@@ -22,12 +22,12 @@ Queue.prototype.dequeue = function() {
   return element
 }
 
-// Time complexity:
+// Time complexity: O(1)
 Queue.prototype.peek = function() {
   return this.storage[this.head]
 }
 
-// Time complexity:
+// Time complexity: O(1)
 Queue.prototype.count = function() {
   return this.tail - this.head
 }
