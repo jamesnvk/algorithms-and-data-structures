359450b4bc2bd72c9566f33a6e9fdaa69e774cbd
James Novak <jamesnovak90@gmail.com>
2017-02-12 18:29:39 -0500

    add forEach and print methods

--- data-structures/linkedlist.js
+++ data-structures/linkedlist.js
@@ -6,4 +6,21 @@ function Node(value) {
 function LinkedList(headValue) {
   if (headValue === undefined) console.log('Must provide value for first node');
   this.head = new Node(headValue);
+  this.tail = this.head
+}
+
+LinkedList.prototype.forEach = function(fn) {
+  var node = this.head
+  while(node) {
+    fn(node.value)
+    node = node.next
+  }
+}
+
+LinkedList.prototype.print = function() {
+  var result = []
+  this.forEach(function(value){
+    result.push(value)
+  })
+  return result.join(', ')
 }
