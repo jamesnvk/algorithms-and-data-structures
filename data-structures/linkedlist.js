37b908f94bfc4f9e1878257f28aa4d4ce79db1f8
James Novak <jamesnovak90@gmail.com>
2017-02-12 20:56:09 -0500

    add insert method

--- data-structures/linkedlist.js
+++ data-structures/linkedlist.js
@@ -24,3 +24,13 @@ LinkedList.prototype.print = function() {
   })
   return result.join(', ')
 }
+
+LinkedList.prototype.insertAfter = function(node, value) {
+  var oldNext = node.next
+  var newNext = new Node(value)
+  node.next = newNext
+  newNext.next = oldNext
+
+  if (this.tail === node) this.tail = newNext;
+  return newNext
+}
