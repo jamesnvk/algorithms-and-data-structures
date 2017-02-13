2c6f8a683adec7374f7ea5ea22311da4e3542f8f
James Novak <jamesnovak90@gmail.com>
2017-02-13 16:34:44 -0500

    implement tree

---
+++ data-structures/tree.js
@@ -0,0 +1,18 @@
+function Tree(value) {
+  this.value = value
+  this.children = []
+}
+
+Tree.prototype.addChild = function(value) {
+  var child = new Tree(value)
+  this.children.push(child)
+  return child
+}
+
+Tree.prototype.contains = function(value) {
+  if(this.value === value) return true;
+  for(let i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(value)) return true;
+  }
+  return false
+}
