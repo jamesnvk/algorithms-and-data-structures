97c16283b5acdaf65cda2b3c31442e5e27f62a74
James Novak <jamesnovak90@gmail.com>
2017-03-01 13:26:17 -0500

    implement graph

---
+++ data-structures/graph.js
@@ -0,0 +1,24 @@
+// graph = {
+//   1: [2, 4],
+//   2: [1, 3, 4],
+//   3: [2],
+//   4: [1, 2]
+// }
+
+function Graph() {
+  this.nodes = {}
+}
+
+Graph.prototype.addNode = function(value) {
+  if(value === undefined) return "Node must contain a value";
+  if(!this.nodes[value]) this.nodes[value] = [];
+}
+
+Graph.prototype.addEdge = function(value1, value2) {
+  if(this.nodes[value1] && this.nodes[value2]) {
+    this.nodes[value1].push(value2)
+    this.nodes[value2].push(value1)
+  } else {
+    return "Invalid node value"
+  }
+}
