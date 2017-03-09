667acc84689d64f771f8f1eb0fbdb6ba4ea2fbf4
James Novak <jamesnovak90@gmail.com>
2017-03-09 16:45:50 -0500

    implement fibonacci recursively

---
+++ recursion/fibonacci.js
@@ -0,0 +1,12 @@
+/*
+Write a function that outputs the nth Fibonnaci number. A number in this sequence is found by adding up the two numbers before it.
+Fibonnaci's sequence:
+input    0 1 2 3 4 5 6  7  8  9 ...
+output   0 1 1 2 3 5 8 13 21 34 ...
+*/
+
+function fibonacci(n) {
+  if(n === 0 || n === 1) return n;
+
+  return fibonacci(n - 1) + fibonacci(n - 2)
+}
