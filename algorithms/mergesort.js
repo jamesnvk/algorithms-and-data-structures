b46f0631582d95c27d90e9ddd749ce39eeed7c51
James Novak <jamesnovak90@gmail.com>
2017-03-31 17:16:37 -0400

    setup left and right sub arrays

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -1,5 +1,9 @@
 function mergeSort(arr) {
   if(arr.length <= 1) return arr;
 
-  
+  //divide and conquer
+  var mid = arr.length / 2
+  var left = arr.slice(0, mid)
+  var right = arr.slice(mid)
+
 }
