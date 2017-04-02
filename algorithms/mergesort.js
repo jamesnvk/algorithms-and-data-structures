64a7d9da5cbc204df95739ab4035aed59b9535b3
James Novak <jamesnovak90@gmail.com>
2017-04-02 17:47:17 -0400

    implement merge routine

--- algorithms/mergesort.js
+++ algorithms/mergesort.js
@@ -11,3 +11,19 @@ function mergeSort(arr) {
 
   return merge(leftSorted, rightSorted)
 }
+
+function merge(left, right) {
+  var result = [], lPointer = 0, rPointer = 0;
+
+  while(result.length < (left.length + right.length)){
+     // if all elements in left have been added, then add remaining right elements
+     if(lPointer === left.length) result = result.concat(right.slice(rPointer));
+     // if all elements in right have been added, then add remaining left elements
+     else if(rPointer === right.length) result = result.concat(left.slice(lPointer));
+     // compare elements in subarrays and add lower of the two to result
+     else if (left[lPointer] <= right[rPointer]) result.push(left[lPointer++]);
+     else result.push(right[rPointer++]);
+  }
+
+  return result
+}
