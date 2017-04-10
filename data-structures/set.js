80fd72a55f8e52fd903b93e06c979b07a6925fe3
James Novak <jamesnovak90@gmail.com>
2017-04-09 21:33:24 -0400

    implementing add, added count to constructor

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,4 +1,13 @@
+// a set stores unique values
+
 function Set(capacity) {
   this.capacity = capacity
   this.storage = {}
+  this.count = 0
+}
+
+Set.prototype.add = function(value) {
+  if(this.storage[value]){
+    return 'Value already exists in set'
+  }
 }
